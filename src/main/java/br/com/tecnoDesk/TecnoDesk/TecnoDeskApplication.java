0cf4768c93236cf9b6a9e0a032bea75795863c17
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-02-25 00:42:33 -0300

    Subindo arquivos do projeto

---
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/TecnoDeskApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecnoDesk.TecnoDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tecnoDesk/TecnoDesk/TecnoDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecnoDesk.TecnoDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnoDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
