98df33ed4d607a6e1240a83c8f6a4bbdfe6ad17a
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-02-26 21:09:40 -0300

    Iniciando criação da classe Colaborador

--- src/main/java/br/com/tecnoDesk/TecnoDesk/Entities/Colaborador.java
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/Entities/Colaborador.java
@@ -1,10 +1,14 @@
 package br.com.tecnoDesk.TecnoDesk.Entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "COLABORADOR")
 public class Colaborador {
 	
 	@Id
