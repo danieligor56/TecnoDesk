824b6cce2a32e586290522c378fb4b2fff46e1f9
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-02-29 09:56:53 -0300

    Atualizando Repositorio

--- src/main/java/br/com/tecnoDesk/TecnoDesk/Entities/OS.java
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/Entities/OS.java
@@ -1,5 +1,6 @@
 package br.com.tecnoDesk.TecnoDesk.Entities;
 
+import br.com.tecnoDesk.TecnoDesk.Enuns.Aparelhos;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -10,8 +11,23 @@ public class OS {
 
 	private long id;
 	
+	private long numOS;
+	
 	private Cliente cliente;
 	
-	private long aparelho;
+	private Aparelhos aparelhos;
+	
+	private String descricaoModelo;
+	
+	private String checkList;
+	
+	private String corpoChamado;
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- src/main/java/br/com/tecnoDesk/TecnoDesk/Enuns/Aparelhos.java
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/Enuns/Aparelhos.java
@@ -2,6 +2,6 @@ package br.com.tecnoDesk.TecnoDesk.Enuns;
 
 public enum Aparelhos {
 	
-	TV,APARELHO_DE_SOM,NOTEBOOK,CELULAR,OUTROS;
+	TV,NOTEBOOK,APARELHO_DE_SOM,CELULAR,OUTROS;
 
 }
