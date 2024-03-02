d2e5ea7da21c8ed5b0834a01615c5d151be859db
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-03-02 15:34:31 -0300

    Criando SERVICE para CLIENTE

--- src/main/java/br/com/tecnoDesk/TecnoDesk/Repository/ClienteRepository.java
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/Repository/ClienteRepository.java
@@ -1,8 +1,10 @@
 package br.com.tecnoDesk.TecnoDesk.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import br.com.tecnoDesk.TecnoDesk.Entities.Cliente;
+
+@Repository
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
---
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/Services/ClienteService.java
@@ -0,0 +1,8 @@
+package br.com.tecnoDesk.TecnoDesk.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+}
