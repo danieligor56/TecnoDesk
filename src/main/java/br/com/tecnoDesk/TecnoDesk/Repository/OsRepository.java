07c3a3d7bcfc72226099e6da0aace12f7f22a2e0
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-03-10 13:52:54 -0300

    Criando validações SE Colaborador é técnico e SE ele existe

--- src/main/java/br/com/tecnoDesk/TecnoDesk/Repository/OsRepository.java
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/Repository/OsRepository.java
@@ -3,7 +3,6 @@ package br.com.tecnoDesk.TecnoDesk.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.tecnoDesk.TecnoDesk.Entities.OS;
-import jakarta.persistence.Id;
 
 @Repository
 
--- src/main/java/br/com/tecnoDesk/TecnoDesk/Services/OsService.java
+++ src/main/java/br/com/tecnoDesk/TecnoDesk/Services/OsService.java
@@ -1,46 +1,56 @@
 package br.com.tecnoDesk.TecnoDesk.Services;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import org.apache.coyote.BadRequestException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import br.com.tecnoDesk.TecnoDesk.DTO.OsDTO;
 import br.com.tecnoDesk.TecnoDesk.Entities.OS;
 import br.com.tecnoDesk.TecnoDesk.Enuns.Ocupacao;
+import br.com.tecnoDesk.TecnoDesk.Repository.ColaboradorRespository;
 import br.com.tecnoDesk.TecnoDesk.Repository.OsRepository;
 
 @Service
 public class OsService {
-	
+
 	@Autowired
 	ModelMapper modelMapper;
-	
+
 	@Autowired
 	OsRepository osRepository;
-		
+	
+	@Autowired
+	ColaboradorRespository colaboradorRespository;
+
 	public OS crianova(OsDTO osDTO) throws BadRequestException {
-	
-	if(osDTO.getColaborador().getOcupacao() != Ocupacao.TECNICO) {
-		
-	throw new BadRequestException("O colaborador selecionado não é um técnico");
-	
+			
+		if (colaboradorRespository.findItById(osDTO.getColaborador().getId()) == null) {
+			
+			throw new BadRequestException("O colaborador selecionado não existe");
+				
+		}else {
+
+			if (osDTO.getColaborador().getOcupacao() != Ocupacao.TECNICO) {
+			
+				throw new BadRequestException("O colaborador selecionado não é um técnico");
+			}
+			
+			else {
+
+					OS novaOs = modelMapper.map(osDTO, OS.class);
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+					LocalDate now = LocalDate.now();
+					formatter.format(now);
+					novaOs.setDataAbertura(formatter.format(now));
+					osRepository.save(novaOs);
+
+				}
+
+			}
+			
+		return null;
 	}
-	
-	OS novaOs =	modelMapper.map(osDTO, OS.class);
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	LocalDate now = LocalDate.now();
-	formatter.format(now);
-	novaOs.setDataAbertura(formatter.format(now));
-	osRepository.save(novaOs);
-	
-	return null;
-	}
-	
-	
 
 }
