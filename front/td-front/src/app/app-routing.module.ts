c3d9f3bef1db55f4bcbadf86b0c864e71963bd0f
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-03-20 21:09:25 -0300

    Rota para Colaborador-list

--- front/td-front/src/app/app-routing.module.ts
+++ front/td-front/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { ColaboradorListComponent } from './components/colaborador/colaborador-list/colaborador-list.component';
 
 const routes: Routes = [
   {
     path:'',component:NavComponent,children:[
-      {path:'home',component:HomeComponent}
+      {path:'home',component:HomeComponent},
+      {path:'colaborador',component:ColaboradorListComponent}
     ]
   }
 ];
